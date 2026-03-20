97645f9dfe5650c59825be966c3ffae39f8ba4cd
Pipip-zen <rafif.nuha@gmail.com>
2026-03-20 17:47:50 +0700

    Fix LocaleDataException by initializing date formatting for id_ID

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'core/constants/app_theme.dart';
 import 'data/database/app_database.dart';
 import 'data/database/seeder.dart';
@@ -8,6 +9,7 @@ import 'presentation/screens/main_shell.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   
   final db = AppDatabase();
   GetIt.instance.registerSingleton<AppDatabase>(db);
